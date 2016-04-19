636ff574765e6180ac49b36ac9cd9d09ce02c841
Michael Spires <Michael.J.Spires@gmail.com>
2016-04-18 20:54:46 -0400

    Fixed payload formatting issue

--- app/src/main/java/com/example/eo/MqttHandler.java
+++ app/src/main/java/com/example/eo/MqttHandler.java
@@ -111,7 +111,7 @@ public class MqttHandler implements MqttCallback {
             Long tsLong = System.currentTimeMillis()/1000;
             String ts = tsLong.toString();
             String msg = "{\"timestamp\":" + ts + ",\"latitude\":"+latitude+"," +
-                    "\"longitude\":"+longitude+ ", \"speed\":"+speed+", \"roadName\":"+roadName+"}" +
+                    "\"longitude\":"+longitude+ ", \"speed\":"+speed+", \"roadName\":\""+roadName+"\"," +
                     "\"bearing\":"+bearing+ ", \"accel\":" + Arrays.toString(accel)+"}";
             Log.d(TAG, ".publish() - Publishing " + msg);
             MqttMessage mqttMsg = new MqttMessage(msg.getBytes());
